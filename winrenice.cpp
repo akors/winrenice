e8cd60dfe6598eaffaebc284fd631b94cab3864e
Alexander Korsunsky <fat.lobyte9@gmail.com>
2012-07-31 12:59:48 +0200

    Write help and version strings
    - Write help and version strings
    - Don't display the whole help on an argument parsing error
    - Accept /? as a flag requesting help
    - Add link to github in version string

--- winrenice.cpp
+++ winrenice.cpp
@@ -230,7 +230,7 @@ bool read_args(ProgramOptions& po, int argc, char *argv[])
     unsigned positional = 0;
     for (int currArg = 1; currArg < argc ; ++currArg)
     {    
-        if (get_switch(argv[currArg]) == 'h')
+        if (get_switch(argv[currArg]) =='h' || get_switch(argv[currArg]) =='?')
         {
             po.show_help = true;
             return true; // stop parsing after help flag
@@ -472,13 +472,79 @@ void incremental_renice_one(
 }
 
 void display_help()
-{}
+{
+    std::cout<<
+R"(WINRENICE [/H | /?] [/V] [/I | /D] <priority> [/E] <target>
+
+Description:
+    This program changes the priority class for existing processes.
+    Processes can be specified with a numerical Process ID or by the executable
+    filename.
+    The priority can increased, decreased or set to a specific value.
+
+Parameter list:
+    <priority>      If the /I or /D flags are ommited, this the target 
+                    process(es) will be set to this priority class. The priority
+                    class can be one of:
+                        IDLE
+                        BELOWNORMAL
+                        NORMAL
+                        ABOVENORMAL
+                        HIGH
+                        REALTIME
+                        
+                    If the either the /I or /D flag is present, <priority> is
+                    interpreted as an integer and the priority class of the 
+                    target process(es) will be increased or decreased by
+                    <priority> steps.
+                        
+    <target>        Denotes the target process or processes.
+                    With the /E flag absent, <target> is the numerical 
+                    process ID of the target process.
+                    With the /E flag present, <target> is the basename of an
+                    executable file. Then, the priority of all processes that
+                    were started by this executable file will be changed.
+    
+    /E              Interpret <target> as the basename of an executable instead
+                    of a process ID
+    
+    /I              Increase priority by <priority> steps
+    
+    /D              Decrease priority by <priority> steps
+
+    /H or /?        Display program help
+    
+    /V              Display program version
+    
+Notes:
+    For certain processes (such as system processes), this utility needs
+    elevated access privileges or the operation will fail.
+    
+    The 32-bit version of this utility cannot be used on 64-bit processes.
+    Please download the appropriate version for your operating system from
+    https://github.com/fat-lobyte/winrenice/downloads .
+
+Examples:
+    Set priority of process 2044 to IDLE:
+        winrenice IDLE 2044
+        
+    Set priority of all processes started by "cmd.exe" to HIGH:
+        winrenice HIGH /E cmd.exe
+    
+    Decrease priority of all processes that were started by svchost.exe by 2:
+        winrenice /D 2 /E svchost.exe
+)";
+
+}
 
 #define PROGRAM_VERSION "0.1"
 
 void display_version()
 {
-    std::cout<<"winrenice version "<<PROGRAM_VERSION<<" by Alexander Korsunsky\n";
+    std::cout<<"winrenice version "<<PROGRAM_VERSION
+        <<" by Alexander Korsunsky\n"
+        <<"See https://github.com/fat-lobyte/winrenice/ for license and "
+          "additional\ninformation.\n";
 }
 
 int main(int argc, char *argv[])
@@ -487,7 +553,7 @@ int main(int argc, char *argv[])
 
     if (!read_args(po, argc, argv))
     {
-        display_help();
+        std::cout<<"Pass the /H flag for help on the usage.\n";
         return EXIT_FAILURE;
     }
     
